c98bc24d5a66d4805f1f025369b8f0a5862e9881
Unknown <pjrader1@yahoo.com>
2017-11-16 19:53:07 -0800

    Add base GameObject

---
+++ CptS122_PA9/GameObject.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <SFML/Graphics.hpp>
+#include <vector>
+
+/**
+Base game object class that all other
+objects in the game extend from.
+*/
+class GameObject {
+public:
+	/**
+	Updates the object.
+	@param gameObjects Global list of game objects.
+	@param dt Time since last frame (change in time since last frame).
+	*/
+	virtual void update(std::vector<GameObject*>& gameObjects, const sf::Time& dt) = 0;
+	/**
+	Draws the object to the window.
+	@param window SFML Window to draw to.
+	*/
+	virtual void draw(sf::RenderWindow& window) = 0;
+
+	/**
+	Returns the position of the object.
+	@return Position of the object.
+	*/
+	virtual const sf::Vector2f& getPosition() const = 0;
+	/**
+	Returns the rotation of the object, in degrees.
+	@return Rotation of the object.
+	*/
+	virtual const float& getRotation() const = 0;
+	/**
+	Returns the bounding box of the object for collision
+	detection.
+	@return Bounding box of the object.
+	*/
+	virtual const sf::FloatRect& getBoundingBox() const = 0;
+};

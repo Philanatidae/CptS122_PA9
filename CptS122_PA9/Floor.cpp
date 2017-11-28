639a18a2bd457d5f12a2732715c798f3c0027c0b
Unknown <pjrader1@yahoo.com>
2017-11-27 19:52:14 -0800

    Add floor object class

---
+++ CptS122_PA9/Floor.cpp
@@ -0,0 +1,27 @@
+#include "Floor.h"
+
+Floor::Floor(const sf::Vector2f& position, const float& width, const float& height)
+	:_floorEntity(sf::Vector2f(width, height)) {
+	_floorEntity.setPosition(position);
+	_floorEntity.setFillColor(sf::Color::White);
+}
+
+void Floor::update(std::vector<GameObject*>& gameObjects, const sf::Time& dt) {
+
+}
+void Floor::draw(sf::RenderWindow &window)
+{
+	window.draw(_floorEntity);
+}
+const sf::Vector2f& Floor::getPosition() const
+{
+	return _floorEntity.getPosition();
+}
+const float& Floor::getRotation() const
+{
+	return _floorEntity.getRotation();
+}
+sf::FloatRect Floor::getBoundingBox() const
+{
+	return _floorEntity.getGlobalBounds();
+}

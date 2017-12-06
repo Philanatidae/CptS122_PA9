c6c45cc9ec4374e939e1e53f90faa8ea1cd1ac3b
Unknown <singhnav314@gmail.com>
2017-12-05 22:53:08 -0800

    Adds ShootingEnemy class
    Enemy shoots left.

---
+++ CptS122_PA9/ShootingEnemy.cpp
@@ -0,0 +1,44 @@
+#include "ShootingEnemy.h"
+
+ShootingEnemy::ShootingEnemy(const sf::Color& color, const sf::Vector2f& position)
+	: _rectShape(sf::Vector2f(SHOOTING_ENEMY_WIDTH, SHOOTING_ENEMY_HEIGHT)),
+	_shootTimer(SHOOTING_ENEMY_SHOOT_PERIOD)
+{
+	_rectShape.setFillColor(color);
+	_rectShape.setPosition(position);
+}
+void ShootingEnemy::update(std::set<GameObject*>& gameObjects, const sf::Time& dt)
+{
+	_shootTimer -= dt.asSeconds();
+
+	// Shoot every second
+	if (_shootTimer <= 0) 
+	{
+		_shootTimer = SHOOTING_ENEMY_SHOOT_PERIOD; // reset shooting timer
+
+		gameObjects.insert(new Projectile(SHOOTING_ENEMY_PROJECTILE_RADIUS, getPosition(), 
+			-1.0f, Target(player)));
+	}
+	// Update Physics
+	FallingPhysicsObject::update(gameObjects, dt);
+}
+void ShootingEnemy::draw(sf::RenderWindow& window)
+{
+	window.draw(_rectShape);
+}
+const sf::Vector2f& ShootingEnemy::getPosition() const
+{
+	return _rectShape.getPosition();
+}
+const float& ShootingEnemy::getRotation() const
+{
+	return _rectShape.getRotation();
+}
+sf::FloatRect ShootingEnemy::getBoundingBox() const
+{
+	return _rectShape.getGlobalBounds();
+}
+void ShootingEnemy::move(const float& dx, const float& dy) 
+{
+	_rectShape.move(dx, dy);
+};
---
+++ CptS122_PA9/ShootingEnemy.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "Enemy.h"
+#include "FallingPhysicsObject.h"
+#include "Projectile.h"
+
+#define SHOOTING_ENEMY_WIDTH 100.0f
+#define SHOOTING_ENEMY_HEIGHT 100.0f
+#define SHOOTING_ENEMY_PROJECTILE_RADIUS 25.0f
+
+#define SHOOTING_ENEMY_SHOOT_PERIOD 1
+
+class ShootingEnemy : public Enemy, public FallingPhysicsObject
+{
+	public:
+		ShootingEnemy(const sf::Color& color, const sf::Vector2f& position);
+
+		void update(std::set<GameObject*>& gameObjects, const sf::Time& dt);
+		void draw(sf::RenderWindow& window);
+		const sf::Vector2f& getPosition() const;
+		const float& getRotation() const;
+		sf::FloatRect getBoundingBox() const;
+
+	protected:
+		void move(const float& dx, const float& dy);
+
+	private:
+		sf::RectangleShape _rectShape;
+		float _shootTimer;
+};

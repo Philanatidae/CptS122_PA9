fd031c03a43254e9997dd4ace8039a37b51c2d63
Unknown <pjrader1@yahoo.com>
2017-12-06 20:05:11 -0800

    Add comments to win game state

--- CptS122_PA9/WinGameState.cpp
+++ CptS122_PA9/WinGameState.cpp
@@ -1,13 +1,24 @@
 #include "WinGameState.h"
 
+/**
+Constructs a game state for winning the game.
+@param game Pointer to game class.
+*/
 WinGameState::WinGameState(Game*const& game)
 	:_winCountdown(WIN_COUNTDOWN_TIME), GameState(game) {
 }
 
+/**
+Initializes the game state.
+*/
 void WinGameState::init() {
 	_gameObjects.insert(new TextObject(sf::Vector2f(100, 100), "You win!", sf::Color::White, 50));
 }
 
+/**
+Updates the game state.
+@param dt Change in time since last frame.
+*/
 void WinGameState::update(const sf::Time& dt) {
 	_winCountdown -= dt.asSeconds();
 	if (_winCountdown <= 0) {
@@ -18,6 +29,10 @@ void WinGameState::update(const sf::Time& dt) {
 	GameState::update(dt);
 }
 
+/**
+Draws the game state.
+@param window Window to render to.
+*/
 void WinGameState::draw(sf::RenderWindow& window) {
 	window.setView(window.getDefaultView());
 	GameState::draw(window);
--- CptS122_PA9/WinGameState.h
+++ CptS122_PA9/WinGameState.h
@@ -10,13 +10,31 @@ class GameState;
 
 #define WIN_COUNTDOWN_TIME 6.0f
 
+/**
+Game state for winning the game.
+*/
 class WinGameState : public GameState {
 public:
+	/**
+	Constructs a game state for winning the game.
+	@param game Pointer to game class.
+	*/
 	WinGameState(Game*const& game);
 
+	/**
+	Initializes the game state.
+	*/
 	void init();
 
+	/**
+	Updates the game state.
+	@param dt Change in time since last frame.
+	*/
 	void update(const sf::Time& dt);
+	/**
+	Draws the game state.
+	@param window Window to render to.
+	*/
 	void draw(sf::RenderWindow& window);
 
 private:

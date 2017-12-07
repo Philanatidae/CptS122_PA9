5839b0305ccd005e47f6ff4275c7f3650de94c77
Unknown <pjrader1@yahoo.com>
2017-12-06 20:12:04 -0800

    Add comments to level builder

--- CptS122_PA9/LevelBuilder.cpp
+++ CptS122_PA9/LevelBuilder.cpp
@@ -1,10 +1,18 @@
 #include "LevelBuilder.h"
 
+/**
+Constructs a level builder.
+@param gameObjectsPtr Pointer to set of game objects (non-owning).
+*/
 LevelBuilder::LevelBuilder(std::set<GameObject*>* const & gameObjectsPtr)
 	:_gameObjectsPtr(gameObjectsPtr),_currPosition(0, 0) {
 
 }
 
+/**
+Draws the level horizontally.
+@param dx Change in x direction.
+*/
 void LevelBuilder::drawHorizontal(const float& dx) {
 	sf::Vector2f position = _currPosition;
 	float ddx = dx;
@@ -17,6 +25,10 @@ void LevelBuilder::drawHorizontal(const float& dx) {
 	_gameObjectsPtr->insert(new Floor(position, ddx, FLOOR_THICKNESS));
 	_currPosition.x += dx;
 }
+/**
+Draws the level vertically.
+@param dy Change in y direction.
+*/
 void LevelBuilder::drawVertical(const float& dy) {
 	sf::Vector2f position = _currPosition;
 	float ddy = dy;
@@ -29,15 +41,28 @@ void LevelBuilder::drawVertical(const float& dy) {
 	_gameObjectsPtr->insert(new Floor(position, FLOOR_THICKNESS, ddy));
 	_currPosition.y += dy;
 }
+/**
+Moves the "cursor" of the level builder without
+drawing.
+@param dx Change in x direction.
+@param dy Change in y direction.
+*/
 void LevelBuilder::move(const float& dx, const float& dy) {
 	_currPosition.x += dx;
 	_currPosition.y += dy;
 }
+/**
+Sets the position of the level builder without drawing.
+*/
 void LevelBuilder::set(const float& x, const float& y) {
 	_currPosition.x = x;
 	_currPosition.y = y;
 }
 
+/**
+Returns the current position of the level builder.
+@return Current position of the level builder.
+*/
 const sf::Vector2f& LevelBuilder::getPosition() const {
 	return _currPosition;
 }
--- CptS122_PA9/LevelBuilder.h
+++ CptS122_PA9/LevelBuilder.h
@@ -6,15 +6,43 @@
 
 #define FLOOR_THICKNESS 20
 
+/**
+Class for building levels.
+*/
 class LevelBuilder {
 public:
+	/**
+	Constructs a level builder.
+	@param gameObjectsPtr Pointer to set of game objects (non-owning).
+	*/
 	LevelBuilder(std::set<GameObject*>* const & gameObjectsPtr);
 
+	/**
+	Draws the level horizontally.
+	@param dx Change in x direction.
+	*/
 	void drawHorizontal(const float& dx);
+	/**
+	Draws the level vertically.
+	@param dy Change in y direction.
+	*/
 	void drawVertical(const float& dy);
+	/**
+	Moves the "cursor" of the level builder without
+	drawing.
+	@param dx Change in x direction.
+	@param dy Change in y direction.
+	*/
 	void move(const float& dx, const float& dy);
+	/**
+	Sets the position of the level builder without drawing.
+	*/
 	void set(const float& x, const float& y);
 
+	/**
+	Returns the current position of the level builder.
+	@return Current position of the level builder.
+	*/
 	const sf::Vector2f& getPosition() const;
 
 private:

8f9a8202e4d8cded00da10487f2bd39d6677e6cf
Unknown <pjrader1@yahoo.com>
2017-12-06 18:57:39 -0800

    Add comments to game over game state

--- CptS122_PA9/GameOverGameState.cpp
+++ CptS122_PA9/GameOverGameState.cpp
@@ -1,13 +1,24 @@
 #include "GameOverGameState.h"
 
+/**
+Constructs a game over game state.
+@param game Pointer to game class.
+*/
 GameOverGameState::GameOverGameState(Game*const& game)
 	:_gameOverCountdown(GAME_OVER_COUNTDOWN_TIME), GameState(game) {
 }
 
+/**
+Initializes the game state.
+*/
 void GameOverGameState::init() {
 	_gameObjects.insert(new TextObject(sf::Vector2f(100, 100), "Game Over!", sf::Color::White, 50));
 }
 
+/**
+Updates the game state.
+@param dt Change in time since last frame.
+*/
 void GameOverGameState::update(const sf::Time& dt) {
 	_gameOverCountdown -= dt.asSeconds();
 	if (_gameOverCountdown <= 0) {
@@ -18,6 +29,10 @@ void GameOverGameState::update(const sf::Time& dt) {
 	GameState::update(dt);
 }
 
+/**
+Draws the game state.
+@param window Window to render to.
+*/
 void GameOverGameState::draw(sf::RenderWindow& window) {
 	window.setView(window.getDefaultView());
 	GameState::draw(window);
--- CptS122_PA9/GameOverGameState.h
+++ CptS122_PA9/GameOverGameState.h
@@ -10,13 +10,31 @@ class GameState;
 
 #define GAME_OVER_COUNTDOWN_TIME 6.0f
 
+/**
+Game state for when the game is over.
+*/
 class GameOverGameState : public GameState {
 public:
+	/**
+	Constructs a game over game state.
+	@param game Pointer to game class.
+	*/
 	GameOverGameState(Game*const& game);
 	
+	/**
+	Initializes the game state.
+	*/
 	void init();
 
+	/**
+	Updates the game state.
+	@param dt Change in time since last frame.
+	*/
 	void update(const sf::Time& dt);
+	/**
+	Draws the game state.
+	@param window Window to render to.
+	*/
 	void draw(sf::RenderWindow& window);
 
 private:

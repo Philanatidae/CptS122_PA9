f1227443a5a01f61f8476d6dc51f60475b1113a6
Unknown <pjrader1@yahoo.com>
2017-12-06 18:54:33 -0800

    Add comments to the base game state

--- CptS122_PA9/GameState.cpp
+++ CptS122_PA9/GameState.cpp
@@ -1,5 +1,9 @@
 #include "GameState.h"
 
+/**
+Constructs a game state.
+@param game Pointer to game class.
+*/
 GameState::GameState(Game*const& game):_gamePtr(game) {
 }
 GameState::~GameState() {
@@ -10,18 +14,34 @@ GameState::~GameState() {
 	}
 }
 
+/**
+Updates the game state.
+@param dt Change in time since last frame.
+*/
 void GameState::update(const sf::Time& dt) {
 	updateAll(dt);
 	deleteObjects();
 }
+/**
+Draws the game state.
+@param window Window to render to.
+*/
 void GameState::draw(sf::RenderWindow& window) {
 	drawAll(window);
 }
 
+/**
+Returns the pointer to the game class.
+@return Pointer to the game class.
+*/
 Game*const& GameState::getGame() const {
 	return _gamePtr;
 }
 
+/**
+Updates all the entities.
+@param dt Change in time since last frame.
+*/
 void GameState::updateAll(const sf::Time& dt) {
 	// While loop is necessary otherwise an invalid iterator will occur
 	std::set<GameObject*>::iterator iterator = _gameObjects.begin();
@@ -30,6 +50,10 @@ void GameState::updateAll(const sf::Time& dt) {
 		gameObject->update(_gameObjects, dt);
 	}
 }
+/**
+Draws all the entities.
+@param window Window to render to.
+*/
 void GameState::drawAll(sf::RenderWindow& window) {
 	for (std::set<GameObject*>::iterator iterator = _gameObjects.begin();
 		iterator != _gameObjects.end();
@@ -39,6 +63,9 @@ void GameState::drawAll(sf::RenderWindow& window) {
 		gameObject->draw(window);
 	}
 }
+/**
+Deletes objects that have their delete flag set.
+*/
 void GameState::deleteObjects() {
 	std::set<GameObject*>::iterator iterator = _gameObjects.begin();
 	std::set<GameObject*>::iterator temp;
--- CptS122_PA9/GameState.h
+++ CptS122_PA9/GameState.h
@@ -9,16 +9,38 @@ class Game; // Forward declare
 #include "GameObject.h"
 #include "Player.h"
 
+/**
+A base class for the game state.
+*/
 class GameState {
 public:
+	/**
+	Constructs a game state.
+	@param game Pointer to game class.
+	*/
 	GameState(Game*const& game);
 	virtual ~GameState();
 
+	/**
+	Initializes the game state.
+	*/
 	virtual void init() = 0;
 
+	/**
+	Updates the game state.
+	@param dt Change in time since last frame.
+	*/
 	virtual void update(const sf::Time& dt);
+	/**
+	Draws the game state.
+	@param window Window to render to.
+	*/
 	virtual void draw(sf::RenderWindow& window);
 
+	/**
+	Returns the pointer to the game class.
+	@return Pointer to the game class.
+	*/
 	Game*const& getGame() const;
 
 protected:
@@ -27,7 +49,18 @@ protected:
 private:
 	Game* const _gamePtr;
 
+	/**
+	Updates all the entities.
+	@param dt Change in time since last frame.
+	*/
 	void updateAll(const sf::Time& dt);
+	/**
+	Draws all the entities.
+	@param window Window to render to.
+	*/
 	void drawAll(sf::RenderWindow& window);
+	/**
+	Deletes objects that have their delete flag set.
+	*/
 	void deleteObjects();
 };
